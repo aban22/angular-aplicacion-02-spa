bb1d6e709f091ccd21504c58492da5dc1f2683c7
Abel Fernandez <abelfergodevelop@gmail.com>
2019-02-10 23:07:53 +0100

    SPA

---
+++ 02-spa/src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-buscador',
+    templateUrl: './buscador.component.html'
+})
+export class BuscadorComponent implements OnInit {
+
+    heroes: Heroe[] = [];
+    termino: string;
+    constructor(private activatedRoute: ActivatedRoute, private heroesService: HeroesService, private router: Router) {
+
+    }
+
+    ngOnInit() {
+        console.log("aa");
+        this.activatedRoute.params.subscribe(
+            params => {
+                this.heroes = this.heroesService.buscarHeroes(params['termino']);
+                this.termino = params['termino'];
+            }
+        );
+    }
+
+    verHeroe(index: number) {
+        this.router.navigate(['/heroe', index]);
+    }
+
+}
